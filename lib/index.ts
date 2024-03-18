6d2412b521b44c528cf440031542580aae648b64
kolirt <kolirt@pm.me>
2024-03-18 12:24:46 +0200

    Update prettier configuration and organize imports.Formatted code and added prettierignore file.

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,9 @@
 import ModalTarget from './components/ModalTarget.vue'
 
+export { closeAllModals, closeModal, confirmModal, openModal } from './actions'
 export { useLock } from './composables/useLock'
-
-export { openModal, confirmModal, closeModal, closeAllModals } from './actions'
-
 export { isOpened, modals } from './data'
-
-export { $on, $off } from './event'
-
+export { $off, $on } from './event'
 export { createModal } from './plugin'
-
 export { Events } from './types'
-
 export { ModalTarget }
--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,39 +1,36 @@
-import {defineConfig} from 'vite'
-import {resolve} from 'path'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import dts from 'vite-plugin-dts'
 import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        cssInjectedByJsPlugin({ useStrictCSP: true }),
-        dts({
-            tsConfigFilePath: './tsconfig.lib.json',
-            rollupTypes: true
-        }),
-        eslint()
-    ],
-    build: {
-        lib: {
-            name: 'vue-modal',
-            formats: ['es', 'umd'],
-            entry: resolve(__dirname, 'lib/index.ts'),
-            fileName: 'vue-modal'
-        },
-        emptyOutDir: true,
-        rollupOptions: {
-            external: [
-                'vue',
-                'js-event-bus',
-            ],
-            output: {
-                globals: {
-                    'vue': 'vue',
-                    'js-event-bus': 'jsEventBus',
-                }
-            }
+  plugins: [
+    vue(),
+    cssInjectedByJsPlugin({ useStrictCSP: true }),
+    dts({
+      tsConfigFilePath: './tsconfig.lib.json',
+      rollupTypes: true
+    }),
+    eslint()
+  ],
+  build: {
+    lib: {
+      name: 'vue-modal',
+      formats: ['es', 'umd'],
+      entry: resolve(__dirname, 'lib/index.ts'),
+      fileName: 'vue-modal'
+    },
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['vue', 'js-event-bus'],
+      output: {
+        globals: {
+          vue: 'vue',
+          'js-event-bus': 'jsEventBus'
         }
+      }
     }
+  }
 })
